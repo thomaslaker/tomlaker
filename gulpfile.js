a217ca7f0a2601dbdb892770cd3d62903c3d5de6
Tom Laker <hello@tomlaker.co.uk>
2015-08-30 15:45:41 +0100

    fixed images on hero & added gulp for optimisation of svg

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    svgstore = require('gulp-svgstore'),
+    path = require('path'),
+    rsp = require('remove-svg-properties').stream;
+
+
+gulp.task('svg', function(){
+    return gulp.src('source/images/icons/*.svg')
+    .pipe(svgstore({ inlineSvg: true }))
+    .pipe(rsp.remove({
+        properties: [rsp.PROPS_FILL]
+    }))
+    .pipe(gulp.dest('source/images/'));
+});
+
+
+gulp.task('watch', function(){
+    gulp.watch('source/images/*.svg', ['svg']);
+});
+
+gulp.task('default', ['svg'], function(){
+
+});
