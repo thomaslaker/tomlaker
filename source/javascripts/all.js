378d1d6d093b20f66ce0e446e1bed615f94135df
Tom Laker <hello@tomlaker.co.uk>
2015-08-30 17:12:51 +0100

    added js for welcome message

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,5 +1,19 @@
 //= require jquery/dist/jquery
 
 $(document).ready(function(){
+    var hour = new Date().getHours();
 
+    function welcomeMsg(msg) {
+        $('#welcome-msg').html(msg);
+    }
+
+    if(hour <= 3) {
+        welcomeMsg('Good Night');
+    } else if(hour >= 4 && hour <= 11) {
+        welcomeMsg('Good Morning');
+    } else if(hour >= 12 && hour <= 17) {
+        welcomeMsg('Good Afternoon');
+    }else if(hour >= 18 && hour <= 24) {
+        welcomeMsg('Good Evening');
+    }
 });
